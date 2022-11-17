97aee370dbb9308dd7f58a1ab9fde942167ab1ca
avbravo <avbravo@avbravo>
2022-11-17 09:15:16 -0500

    capitulo 17

---
+++ src/main/java/com/avbravo/jakartasetverfaces40/jakartasetverfaces40/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package com.avbravo.jakartasetverfaces40.jakartasetverfaces40;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+
+   
+}
